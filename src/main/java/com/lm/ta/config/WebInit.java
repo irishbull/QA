fb5dab83443456ee61ae01c43343e4cd6b07181a
domonaco <P@v!ll!on99>
2018-05-09 11:49:22 +0200

    LMTA-xxxx: PageObject First Example

--- src/main/java/com/lm/ta/config/WebInit.java
+++ src/main/java/com/lm/ta/config/WebInit.java
@@ -1,36 +1,37 @@
 package com.lm.ta.config;
 
-import com.lm.ta.utilities.Constants;
 import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.w3c.dom.Document;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import com.lm.ta.utilities.Constants;
+
+/**
+ * <strong>WebInit</strong> handles setup and teardown of WebDriver.
+ * @author dmonaco
+ */
 public class WebInit {
-	
-	private static final Logger logger = LoggerFactory.getLogger(WebInit.class);
 
-	protected static String url;
 	protected static WebDriver driver;
-	protected static WebDriverWait timeWait;
-	protected static int hightlightTime;
-	protected static int popupLoadWait;
-	protected static int pageLoadWait;
-	protected static int imageLoadWait;
-	protected static Document doc;	
 	
-	protected void Init() {
-		logger.info("::: Opening Chrome Browser");
+	@BeforeClass
+	public static void setUp(){
 		System.setProperty(Constants.CHROME_DRIVER_SYSTEM_PROPERTY, Constants.CHROME_DRIVER_SYSTEM_PATH);
 		driver = new ChromeDriver();
-		//driver.manage().timeouts().implicitlyWait(Constants.IMPLICITY_WAIT, TimeUnit.SECONDS);
-		//timeWait = new WebDriverWait(driver, Constants.EXPLICITY_WAIT);
-		//driver.manage().window().maximize();
-		logger.info("::: Navigate Url: {}",Constants.URL_TO_INVOKE);
-		//driver.get(Constants.URL_TO_INVOKE);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 	}
+	
+	@After
+	public void cleanUp(){
+		driver.manage().deleteAllCookies();
+	}
+	
+	@AfterClass
+	public static void tearDown(){
+		driver.close();
+	}	
 }
--- src/main/java/com/lm/ta/testsuites/TestSuite.java
+++ src/main/java/com/lm/ta/testsuites/TestSuite.java
@@ -3,14 +3,12 @@ package com.lm.ta.testsuites;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import com.lm.ta.testcases.TestCase_001;
-import com.lm.ta.testcases.TestCase_002;
-import com.lm.ta.testcases.TestCase_003;
+
+import com.lm.ta.testcases.SignUpFormTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestCase_001.class,
-	            TestCase_002.class, 
-		        TestCase_003.class })
+@SuiteClasses({ SignUpFormTest.class
+				})
 
 public class TestSuite {
 
