70ed176067b44d83b5210faf93ab256e1e584f7d
Bologna Giovanni <BolognaGi@valueteam.com>
2018-05-09 17:55:55 +0200

    LMTA-xxxx-PageObject: code formatting

--- src/main/java/com/lm/ta/utilities/Constants.java
+++ src/main/java/com/lm/ta/utilities/Constants.java
@@ -1,8 +1,8 @@
 package com.lm.ta.utilities;
 
 public class Constants {
-	
-	public final static String CHROME_DRIVER_SYSTEM_PROPERTY = "webdriver.chrome.driver";
-	public final static String CHROME_DRIVER_SYSTEM_PATH = "driver/chromedriver.exe";
+
+  public final static String CHROME_DRIVER_SYSTEM_PROPERTY = "webdriver.chrome.driver";
+  public final static String CHROME_DRIVER_SYSTEM_PATH = "driver/chromedriver.exe";
 
 }
