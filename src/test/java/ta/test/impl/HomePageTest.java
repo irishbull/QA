8217acc49ff7155ad624c98a12e2951699a4ad06
Giovanni Bologna <giovanni.bologna@nttdata.com>
2018-06-01 10:18:12 +0200

    LMTA-xxxx-OnHover: removed unused import

--- src/test/java/ta/test/impl/HomePageTest.java
+++ src/test/java/ta/test/impl/HomePageTest.java
@@ -2,7 +2,6 @@ package ta.test.impl;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
