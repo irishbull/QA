21d1015c14c75f0854d7eb812ba4455182e4259b
Domenico Monaco <domenicomonaco1983@gmail.com>
2018-06-06 17:04:16 +0200

    LMTA-master: chrome security skip configuration (TO_REMOVE)

--- src/test/java/ta/driver/SeleniumDriver.java
+++ src/test/java/ta/driver/SeleniumDriver.java
@@ -71,6 +71,8 @@ public class SeleniumDriver {
         chrOptions.setExperimentalOption("prefs", chromePrefs);
         chrOptions.addArguments("--disable-plugins", "--disable-extensions",
             "--disable-popup-blocking");
+        // TO REMOVE
+        chrOptions.addArguments("--no-sandbox");
 
         // Chrome desired capabilities
         capabilities = DesiredCapabilities.chrome();
