aec3a0ed51cf11584f881ee53d287ff3a48962d3
Giovanni Bologna <giovanni.bologna@nttdata.com>
2018-08-23 12:48:57 +0200

    LMTA-xxxx-src-main-test-dataprovider-tooso: suggest angular pages

--- src/test/java/ta/test/impl/tooso/ToosoSuggestTest.java
+++ src/test/java/ta/test/impl/tooso/ToosoSuggestTest.java
@@ -16,6 +16,7 @@ import java.util.concurrent.TimeUnit;
 import io.qameta.allure.Description;
 import ta.dataproviders.JSONDataProvider;
 import ta.driver.SeleniumDriver;
+import ta.pageobjects.impl.ToosoSearchAngularPO;
 import ta.pageobjects.impl.ToosoSearchPO;
 import ta.test.ToosoBaseTest;
 import ta.utilities.ReadPropertiesFile;
@@ -30,6 +31,7 @@ public class ToosoSuggestTest extends ToosoBaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(ToosoSuggestTest.class);
 
+    // REACT
     @Test(dataProvider = "fetchJSONData", dataProviderClass = JSONDataProvider.class)
     @Description("GET [type = SUGGEST] - validate requests")
     public void tc_001_verifySuggestRequest(JSONObject testData) throws Exception {
@@ -41,6 +43,50 @@ public class ToosoSuggestTest extends ToosoBaseTest {
         driver.get(ReadPropertiesFile.getProperty("base.url") + testData.get("pathAndQuery").toString());
 
         ToosoSearchPO toosoSearchPO = new ToosoSearchPO();
+
+        toosoSearchPO.clickOnSearchBar();
+
+        String word = testData.get("search").toString();
+
+        toosoSearchPO.enterWord(word);
+
+        // wait for quiescence
+        SeleniumDriver.getInstance().getProxy().waitForQuiescence(QUIET_PERIOD, TIMEOUT, TimeUnit.SECONDS);
+
+        Har har = SeleniumDriver.getInstance().getProxy().getHar();
+
+        List<HarEntry> entriesCaptured = har.getLog().getEntries();
+
+        List<HarEntry> entriesToValidate = ToosoAnalyticsUtils.retrieveEntriesOfType(entriesCaptured, SUGGEST);
+
+        Assert.assertEquals(entriesToValidate.size(), word.length() + 1, "Number of requests [type = SUGGEST] captured by proxy:");
+
+        // check the first entry (q=*)
+        ToosoAnalyticsUtils.checkMandatoryValues(entriesToValidate.get(0).getRequest().getUrl(), testData, SUGGEST);
+        ToosoAnalyticsUtils.checkSuggestQueryParam(entriesToValidate.get(0).getRequest().getUrl(), 1, PROXY_SUGGEST_PREFIX);
+
+        // check the other entries
+        for(int i = 1; i < entriesToValidate.size(); i++) {
+            String url = entriesToValidate.get(i).getRequest().getUrl();
+            logger.info("{} Request [type = {}] to validate -> {}", i, SUGGEST, url);
+            ToosoAnalyticsUtils.checkMandatoryValues(url, testData, SUGGEST);
+            ToosoAnalyticsUtils.checkSuggestQueryParam(url, i, word);
+        }
+    }
+
+
+    // ANGULAR
+    @Test(dataProvider = "fetchJSONData", dataProviderClass = JSONDataProvider.class)
+    @Description("GET [type = SUGGEST] - validate requests")
+    public void tc_002_verifyAngularSuggestRequest(JSONObject testData) throws Exception {
+
+        String description = testData.get("description").toString();
+
+        WebDriver driver = SeleniumDriver.getInstance().getDriver();
+
+        driver.get(ReadPropertiesFile.getProperty("base.url") + testData.get("pathAndQuery").toString());
+
+        ToosoSearchAngularPO toosoSearchPO = new ToosoSearchAngularPO();
 
         toosoSearchPO.clickOnSearchBar();
 
